5b997072b1749d401d751ebc150d97b6ca66c17e
Carl Bush <CarlBushs13@gmail.com>
2022-06-24 15:23:43 -0700

    login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,7 @@ const resolvers = {
             };
             throw new AuthenticationError("Not logged in");
         }
-       
+
     },
 
     Mutation: {
@@ -24,8 +24,24 @@ const resolvers = {
             const token = signToken(user);
 
             return { token, user }
-        }
+        },
 
+        login: async function (parent, { email, password }) {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        }
     }
 
 };
