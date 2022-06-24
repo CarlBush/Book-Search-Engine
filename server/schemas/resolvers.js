c8ad8e59b94236e429df968a0ec98b2773f3791a
Carl Bush <CarlBushs13@gmail.com>
2022-06-24 13:36:29 -0700

    fix resolver query add, me functional graphql test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,15 +2,17 @@ const { User } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
+    Query: {
 
-    me: async function (parent, arg, context) {
-        if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id })
-                .select("-__v -password")
-                .populate("books");
-            return userData;
-        };
-        throw new AuthenticationError("Not logged in");
+        me: async function (parent, arg, context) {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select("-__v -password")
+                    .populate("books");
+                return userData;
+            };
+            throw new AuthenticationError("Not logged in");
+        }
     }
 };
 
