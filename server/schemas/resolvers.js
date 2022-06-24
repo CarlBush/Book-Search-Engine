23710a18824d7d46b896fabbf7d3c62b010c57ad
Carl Bush <CarlBushs13@gmail.com>
2022-06-24 09:41:52 -0700

    server.js update for apollo-server, context and auth not added

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+
+    me: async function (parent, arg, context) {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+                .select("-__v -password")
+                .populate("books");
+            return userData;
+        };
+        throw new AuthenticationError("Not logged in");
+    }
+};
+
+module.exports = resolvers;
